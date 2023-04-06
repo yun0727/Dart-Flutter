93726a7b874c734fddbc8c920fe67dded3973b0e
yun <austru00@gmail.com>
2023-04-06 11:16:14 +0900

    4.8 Inheritance

---
+++ lecture/4CLASSES/8Inheritance.dart
@@ -0,0 +1,27 @@
+class Human {
+  final String name;
+  Human({required this.name});
+  void sayHello() {
+    print('Hi my name is $name');
+  }
+}
+
+enum Team { red, blue }
+
+class Player extends Human {
+  final Team team;
+  Player({
+    required this.team,
+    required String name,
+  }) : super(name: name);
+
+  @override
+  void sayHello() {
+    super.sayHello();
+    print('and I play for ${team}');
+  }
+}
+
+void main() {
+  var player = Player(team: Team.red, name: 'nico');
+}
