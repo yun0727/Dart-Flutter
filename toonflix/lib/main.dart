76ee0325a1096a20adf9d80d3a2c08d95105d5b3
yun <austru00@gmail.com>
2023-04-11 17:49:12 +0900

    3.2 Buttons Section

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -8,13 +8,13 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
           backgroundColor: Color(0xFF181818),
           body: Padding(
               padding: EdgeInsets.symmetric(horizontal: 40),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(
                     height: 80,
@@ -41,6 +41,50 @@ class App extends StatelessWidget {
                             ),
                           )
                         ],
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 120,
+                  ),
+                  Text(
+                    'Total Balance',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white.withOpacity(0.8),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '\$5 194 482',
+                    style: TextStyle(
+                      fontSize: 44,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(45),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 50),
+                          child: Text(
+                            'Transfer',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
                       )
                     ],
                   )
