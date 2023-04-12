e699971d78727ff5a9232f26e9748b99589cbdea
yun <austru00@gmail.com>
2023-04-12 17:33:22 +0900

    4.4 Widget Lifecycle

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +33,12 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [MyLargeTitle()],
+            children: [
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                  onPressed: toggleTitle,
+                  icon: const Icon(Icons.remove_red_eye))
+            ],
           ),
         ),
       ),
@@ -33,10 +46,31 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  //부모 요소에 의존하는 데이터를 초기화 해야하는 경우
+  //initState가 build보다 먼저 실행되어야 한다
+  //단 한번만 호출됨
+  void initState() {
+    super.initState();
+  }
+  //dispose
+  //위젯이 스크린에서 제거될 때 호출되는 메서드
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
